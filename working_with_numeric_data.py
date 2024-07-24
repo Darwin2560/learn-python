56f852ea63cf1d2f68425dd3e7db9b100fe70cf3
darwin ruiz <junior.ruiz.perez00@gmail.com>
2024-07-25 00:30:30 +0200

    feat: working with numeric data

---
+++ working_with_numeric_data.py
@@ -0,0 +1,27 @@
+# Arithmetic Operations:
+# Addition: 3 + 2
+# Subtraction: 3 - 2
+# Multiplication: 3 * 2
+# Division: 3 / 2
+# Floor Division: 3 // 2
+# Exponent: 3 ** 2
+# Modulus: 3 % 2
+
+num = 1
+num += 1
+num_1 = 3
+num_2 = 2
+
+print(dir(num)) # No muestra todos los atributos
+print(abs(-2))
+print(round(3.75, 1))
+
+# Comparisons:
+# Equal: 3 == 2 False
+# Not Equal: 3 != 2 True
+# Greater than: 3 > 2 True
+# Less than: 3 < 2 False
+# Greater or equal: 3 >= 2 True
+# Less or equal: 3 <= 2 False
+
+print(num_1 <= num_2) # False
