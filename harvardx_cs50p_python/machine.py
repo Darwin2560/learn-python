bb62956255b9f27ee2163c4691b80120eee88f9b
darwin ruiz <junior.ruiz.perez00@gmail.com>
2024-07-29 00:23:49 +0200

    feat: the machine say

---
+++ harvardx_cs50p_python/machine.py
@@ -0,0 +1,11 @@
+emoticon = '🫣'
+
+def main():
+    global emoticon
+    say('Is anyone there?')
+    emoticon = '👋'
+    say('Oh, hi!')
+def say(phrase):
+    print(phrase + " " + emoticon)
+
+main()
