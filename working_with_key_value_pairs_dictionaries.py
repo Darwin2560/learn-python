b218d7095fb9486a3f829a6502bc9161c0b7e8b5
darwin ruiz <junior.ruiz.perez00@gmail.com>
2024-07-29 02:08:24 +0200

    feat: working with key pairs dictionaries

---
+++ working_with_key_value_pairs_dictionaries.py
@@ -0,0 +1,21 @@
+student = {
+    'name': 'Darwin',
+    'age': 25,
+    'courses': ['Math', 'CompSci']
+}
+
+student['phone'] = '555-1234'
+student['avatar'] = '👨‍💻'
+student.update({'age': 24})
+del student['phone']
+avatar = student.pop('avatar')
+print(f'Avatar: {avatar}')
+# print(student.get('phone', 'No phone number provided'))
+print(student)
+print(len(student))
+print(student.keys())
+print(student.values())
+print(student.items())
+
+for key, value in student.items():
+    print(key, value)
