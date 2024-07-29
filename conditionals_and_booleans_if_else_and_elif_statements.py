684683382b275c3765c3b1882db006400b975532
darwin ruiz <junior.ruiz.perez00@gmail.com>
2024-07-29 23:31:21 +0200

    feat: conditionals and booleans if - else - elif statements

---
+++ conditionals_and_booleans_if_else_and_elif_statements.py
@@ -0,0 +1,21 @@
+# and, or, not operators
+
+use = 'Admin'
+logged_in = True
+
+if use == 'Admin' and logged_in:
+    print('Access granted')
+else:
+    print('Access denied')
+
+a = [1, 2, 3, 4, 5]
+b = [1, 2, 3, 4, 5]
+print(id(a))
+print(id(b))
+print(a is b) # False, because they are different objects
+print(id(a) == id(b)) # False
+b = a
+print(a is b) # True, because they are the same object
+print(id(a) == id(b)) # True, because they have the same memory address
+
+
