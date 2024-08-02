48b9592528aef9ae43ff0e77df12c25611b60c63
darwin ruiz <junior.ruiz.perez00@gmail.com>
2024-08-02 11:23:56 +0200

    report

---
+++ harvardx_cs50p_python/report.py
@@ -0,0 +1,17 @@
+def main():
+    spacecraft = {'name': 'Voyager 1'}
+    spacecraft.update({'distace': 42.123, 'orbit': 'Sun'})
+    print(create_report(spacecraft))
+
+def create_report(spacecraft):
+    return f"""
+    ========= REPORT =========
+
+    Name: {spacecraft.get('name', 'Unknown' )}
+    Distace: {spacecraft.get('distace', 'Unknown')} AU
+    Orbit: {spacecraft.get('orbit', 'Unknown')}
+
+    ======= END REPORT =======
+    """
+
+main()
