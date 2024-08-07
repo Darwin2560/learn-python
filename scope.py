d1d70009f1014ab3e921c6d568496e60d9137a94
darwin ruiz <junior.ruiz.perez00@gmail.com>
2024-08-07 16:28:36 +0200

    feat: variable globals and rebanar lists tuples

---
+++ scope.py
@@ -0,0 +1,26 @@
+'''
+LEGB
+Local, Enclosing, Global, Built-in
+'''
+
+x = 'global x'
+
+def test():
+    y = 'local y'
+    # print(y)
+    print(x)
+
+test()
+#print(y) # NameError: name 'y' is not defined
+
+def outer():
+    x = 'outer x'
+
+    def inner():
+        x = 'inner x'
+        print(x)
+    inner()
+    print(x)
+
+
+outer()
