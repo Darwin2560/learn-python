5b3496ded61bcf597331ed6d321e49ee54429391
darwin ruiz <junior.ruiz.perez00@gmail.com>
2024-08-09 00:17:32 +0200

    feat: exceptions

---
+++ harvardx_cs50p_python/distances.py
@@ -0,0 +1,25 @@
+distances = {
+    'Voyager 1': '163',
+    'Voyager 2': '334',
+    'Pioneer 10': '80 AU',
+    'New Horizons': '58',
+    'Pioneer 11': '44 AU'
+}
+
+def main():
+    spacecraft = input('Enter the name of a spacecraft: ')
+    try:
+        au = float(distances[spacecraft])
+    except KeyError:
+        print(f'{spacecraft} not found in the Dictionary')
+        return
+    except ValueError:
+        print(f'can\'t convert {distances[spacecraft]} to a float')
+        return
+    m = convert(au)
+    print(f'{m} m away')
+
+def convert(au):
+    return au * 149597870700
+
+main()
