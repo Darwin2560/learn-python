53b70e9b777f73265c76f1adb98ebea85e0f744c
darwin ruiz <junior.ruiz.perez00@gmail.com>
2024-08-10 00:32:44 +0200

    feat: random

---
+++ harvardx_cs50p_python/random.py
@@ -0,0 +1,18 @@
+import random
+
+lits = ['a', 'b', 'c', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+random_letter = random.choice(lits)
+print(random_letter)
+
+print("============================================================")
+
+number = random.randint(1, 100)
+print(number)
+
+print("============================================================")
+
+cards = ['jack', 'queen', 'king', 'ace']
+random.shuffle(cards)
+for card in cards:
+    print(card)
