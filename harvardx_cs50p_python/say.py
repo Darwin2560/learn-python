50c0890b91bb7a1674571d83f0bac5e2ce828f90
jrdarwin <junior.ruiz.perez00@gmail.com>
2024-10-27 23:05:05 +0100

    feat: sayings file

--- harvardx_cs50p_python/say.py
+++ harvardx_cs50p_python/say.py
@@ -1,6 +1,8 @@
 import sys
 
 import cowsay
+from sayings import hello
 
 if len(sys.argv) == 2:
     cowsay.trex("hello, " + sys.argv[1])
+    hello(sys.argv[1])
---
+++ harvardx_cs50p_python/sayings.py
@@ -0,0 +1,15 @@
+def main():
+    hello("world")
+    goodbye("world")
+
+
+def hello(name):
+    print(f"Hello, {name} 👋")
+
+
+def goodbye(name):
+    print(f"Goodbye, {name} 👋")
+
+
+if __name__ == "__main__":
+    main()
